2be1e24792e7ae039f219b790080604fead1e2ce
SachinSardar1891 <156399288+SachinSardar1891@users.noreply.github.com>
2024-02-18 10:34:31 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -7,5 +7,7 @@ Scanner sc=new Scanner(System.in);
 System.out.println("Enter youre Name:");
 String name=sc.next();
 System.out.println(name);
+System.out.println("Everything is up to date");
+  System.out.println("Now you find the collaborator");
 }
 }
