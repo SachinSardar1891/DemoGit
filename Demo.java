a8f09ae16674667348846d515cf348897362c5a4
SachinSardar1891 <sachinsardar1891@gmail.com>
2024-02-18 10:32:15 +0530

    Changes

--- Demo.java
+++ Demo.java
@@ -1,8 +1,11 @@
-
+import java.util.*
 class Demo
 {
 public static void main(String args[])
 {
-
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter youre Name:");
+String name=sc.next();
+System.out.println(name);
 }
 }
